ff9eb474c214e33b266017b6893ce421ac036e33
Tripsy <engine@play-zone.ro>
2026-08-13 01:59:09 +0300

    chore(layout): rename the logo to NReady

--- src/components/layout/logo.default.tsx
+++ src/components/layout/logo.default.tsx
@@ -2,7 +2,7 @@ export function LogoComponent() {
 	return (
 		<div className="flex px-3 py-1 items-center justify-center rounded-xl bg-accent">
 			<span className="text-2xl font-bold text-accent-foreground">
-				StarDMS
+				NReady
 			</span>
 		</div>
 	);
